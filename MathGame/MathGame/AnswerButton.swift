dbcda68ab3e51fcd0ac5f9c6719bfbed38094a28
SOR SOPHEAK <ssopheak2002@gmail.com>
2024-02-12 10:19:59 +0700

    #002 SwiftUI Math Game small project

---
+++ MathGame/MathGame/AnswerButton.swift
@@ -0,0 +1,29 @@
+//
+//  AnswerButton.swift
+//  MathGame
+//
+//  Created by SOR SOPHEAK on 28/1/24.
+//
+
+import SwiftUI
+
+struct AnswerButton: View {
+    var number : Int
+    
+    var body: some View {
+        Text("\(number)")
+            .frame(width: 110, height: 110)
+            .font(.system(size: 40, weight: .bold))
+            .foregroundColor(Color.white)
+            .background(Color.blue)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .padding()
+    }
+}
+
+struct AnswerButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AnswerButton(number: 100)
+    }
+    
+}
---
+++ MathGame/MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by SOR SOPHEAK on 28/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
