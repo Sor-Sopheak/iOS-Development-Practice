7da8ba181d54bc09b58119c213403309922a0239
SOR SOPHEAK <ssopheak2002@gmail.com>
2024-03-14 18:43:57 +0700

    DTR project draft

---
+++ DTRiOSApp/DTRiOSApp/DTRiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DTRiOSAppApp.swift
+//  DTRiOSApp
+//
+//  Created by SOR SOPHEAK on 14/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DTRiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
